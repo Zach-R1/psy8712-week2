46cc3bccc58566e87533e4780e6879924452262c
Zachariah R. Roper <roper063@umn.edu>
2026-02-02 12:26:13 -0600

    Added rt_f_df and fixed order of label and level in rt_df$gender

--- R/week2.R
+++ R/week2.R
@@ -1,4 +1,8 @@
 # Import and Labeling
 rt_df <- read.csv(file = "data/week2.csv", header = TRUE)
 rt_df$condition <- factor(rt_df$condition, levels = c("A", "B"), labels = c("Control", "Experimental"))
-rt_df$gender <- factor(rt_df$gender, levels = c("Male", "Female", "Nonbinary"), labels = c("M", "F", "N"))
+rt_df$gender <- factor(rt_df$gender, levels = c("M", "F", "N"), labels = c("Male", "Female", "Nonbinary"))
+
+# Analysis
+mean(rt_df$rt)
+rt_f_df <- rt_df[rt_df$gender == "Female", ]
